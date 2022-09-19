273b6b5b5d0093f6f4371daa13342a6a0ecbc45e
yaroslav2k <iaroslav2k@gmail.com>
2022-09-19 11:35:38 +0300

    Improve tests and add them to CI

--- pybash/tests/query_parser_spec.py
+++ pybash/tests/query_parser_spec.py
@@ -4,9 +4,12 @@ from pybash.query_parser import QueryParser
 from mamba import description, context, it
 from expects import expect, equal, be_a
 
+from faker import Faker
+
+
 with description("QueryParser") as self:
     with description("#call") as self:
-        with it("mocked test"):
-            result = QueryParser().call("123")
+        with it("returns an instance of `Command` class"):
+            result = QueryParser().call(Faker().pystr())
 
             expect(result).to(be_a(Command))
