7d8eb014d3640491c3f0e432b16d751c9c09f030
Nickkreker <nick_berezikov@me.com>
2022-09-19 22:41:47 +0300

    Fix CatCommand to work with absent files

--- pybash/pybash/command.py
+++ pybash/pybash/command.py
@@ -32,12 +32,13 @@ class EchoCommand(Command):
 class CatCommand(Command):
     def run(self, arguments: list[str]) -> tuple[str, int]:
         output = ""
+        exit_status = 0
         for file_path in arguments:
             if not os.path.exists(file_path):
-                output += f"cat: {file_path}: No such file or directory"
-                return output, 1
+                output += f"cat: {file_path}: No such file or directory\n"
+                exit_status = 1
+            else:
+                with open(file_path) as f:
+                    output += f.read()
 
-            with open(file_path) as f:
-                output += f.read()
-
-        return output, 0
+        return output, exit_status
