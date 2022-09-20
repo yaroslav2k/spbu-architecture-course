fa841baeff1b49b971bf3b86e291d851653274ee
Nickkreker <nick_berezikov@me.com>
2022-09-20 23:45:44 +0300

    Add cli with infinite loop

---
+++ pybash/pybash/cli.py
@@ -0,0 +1,18 @@
+from pybash.executor import Executor
+
+class CLI:
+    def __init__(self):
+        self.executor = Executor()
+
+    def run(self) -> None: 
+        while(True):
+            user_input = input(">: ")
+
+            if (user_input == "exit"):
+                break
+
+            self.executor.call(user_input)    
+
+if __name__ == "__main__":
+    cli = CLI()
+    cli.run()
