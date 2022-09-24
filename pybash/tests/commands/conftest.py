4aea7d19b2d14a3e5e32532cbd64afe6848ccc47
Nickkreker <nick_berezikov@me.com>
2022-09-25 01:20:11 +0300

    Add empty text file fixture

--- pybash/tests/commands/conftest.py
+++ pybash/tests/commands/conftest.py
@@ -3,10 +3,21 @@ import pytest
 
 @pytest.fixture(scope="session")
 def text_file(tmp_path_factory):
-    content = "Hello world!"
+    content = "Hello world!\n12f13q3vvae\n\n{asfaf\ta34 r3\n"
     file_name = "a.txt"
     f = tmp_path_factory.mktemp("data") / file_name
     f.touch()
     f.write_text(content)
 
-    return f, content
+    return str(f), content
+
+
+@pytest.fixture(scope="session")
+def empty_text_file(tmp_path_factory):
+    content = ""
+    file_name = "empty.txt"
+    f = tmp_path_factory.mktemp("data") / file_name
+    f.touch()
+    f.write_text(content)
+
+    return str(f), content
