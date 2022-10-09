918b79e1919224480a0f833e66fc415bafbc7499
Nickkreker <nick_berezikov@me.com>
2022-10-09 18:59:25 +0300

    Finish grep logic

--- pybash/pybash/commands/grep_command.py
+++ pybash/pybash/commands/grep_command.py
@@ -1,5 +1,7 @@
 import argparse
 from dataclasses import dataclass
+import re
+import os
 
 from pybash.commands.base_command import BaseCommand
 from pybash.commands.command_streams import CommandStreams
@@ -47,10 +49,59 @@ class GrepCommand(BaseCommand):
         parsed_arguments = self._ArgumentsParser().parse(arguments)
         self._validate_arguments(parsed_arguments)
 
-        # TODO: Provide required implementation.
-        streams.output.write(f"Invoked grep via {parsed_arguments}\n")
+        pattern = parsed_arguments.search
+        if parsed_arguments.word_regexp:
+            pattern = r"\b" + pattern + r"\b"
 
-        return BaseCommand.EXIT_SUCCESS
+        pattern_compiled = None
+        if parsed_arguments.ignore_case:
+            pattern_compiled = re.compile(pattern, re.IGNORECASE)
+        else:
+            pattern_compiled = re.compile(pattern)
+
+        exit_code = 1
+        num_paragraphs = 0
+        for file_path in parsed_arguments.files:
+            if not os.path.exists(file_path):
+                streams.output.write(f"grep: {file_path}: No such file or directory\n")
+                exit_code = 2
+                continue
+            inside_paragraph = False
+            with open(file_path, "r") as file:
+                last_match_line = None
+                for idx, line in enumerate(file):
+                    match = pattern_compiled.search(line)
+                    if match:
+                        if exit_code != 2:
+                            exit_code = BaseCommand.EXIT_SUCCESS
+                        if num_paragraphs > 0 and not inside_paragraph:
+                            streams.output.write("--\n")
+                        inside_paragraph = True
+                        output = line
+                        if output[-1] != "\n":
+                            output += "\n"
+                        if len(parsed_arguments.files) > 1:
+                            output = file_path + ":" + output
+
+                        last_match_line = idx
+                        streams.output.write(output)
+                    elif None not in (last_match_line, parsed_arguments.after_context):
+                        if idx - last_match_line <= parsed_arguments.after_context:
+                            output = line
+                            if output[-1] != "\n":
+                                output += "\n"
+                            if len(parsed_arguments.files) > 1:
+                                output = file_path + ":" + output
+                            streams.output.write(output)
+                        elif (
+                            idx - last_match_line == parsed_arguments.after_context + 1
+                        ):
+                            num_paragraphs += 1
+                            inside_paragraph = False
+                if None not in (last_match_line, parsed_arguments.after_context):
+                    num_paragraphs += 1
+
+        return exit_code
 
     def _validate_arguments(self, arguments: _ArgumentsParser.Result) -> None:
         if arguments.search is None or not len(arguments.files):
